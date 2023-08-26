84fe26b5faa1f04177f094a7fb043fdb70767548
Ale Gomez <gom20347@uvg.edu.gt>
2023-08-26 15:56:29 -0600

    added more functions

--- screensaver.cpp
+++ screensaver.cpp
@@ -10,3 +10,15 @@ bool initializeSDL() {
 bool initializeTTF() {
     return TTF_Init() == 0;
 }
+
+TTF_Font* loadFont(const char* fontPath, int fontSize) {
+    return TTF_OpenFont(fontPath, fontSize);
+}
+
+SDL_Window* createWindow(const char* title, int width, int height) {
+    return SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN);
+}
+
+SDL_Renderer* createRenderer(SDL_Window* window) {
+    return SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+}
